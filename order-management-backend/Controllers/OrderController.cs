4eb867a137636d820d368705de81225b668d65f5
Sona <sona.pov@Gmail.com>
2024-11-17 11:31:17 +0100

    git init

---
+++ order-management-backend/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using order_management_backend.Models;
+
+namespace order_management_backend.Controllers
+{
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public static List<Order> Orders = new List<Order>(); // static list as database
+
+        [HttpPost("api/orders")]
+        public IActionResult CreateOrder([FromBody] Order order)
+        {
+            order.OrderDate = DateTime.UtcNow;
+            order.Id = Orders.Count + 1;
+            Orders.Add(order);
+            return CreatedAtAction(nameof(GetOrders), new { id = order.Id }, order);
+        }
+
+        [HttpGet("api/orders")]
+        public IActionResult GetOrders([FromQuery] string productName)
+        {
+            var filteredOrders = string.IsNullOrWhiteSpace(productName)
+                ? Orders
+                : Orders.Where(o => o.ProductName.Contains(productName, StringComparison.OrdinalIgnoreCase)).ToList();
+            return Ok(filteredOrders);
+        }
+
+    }
+}
---
+++ order-management-backend/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace order_management_backend.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
